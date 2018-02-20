57df6f6ab7536a7df3d2eab9cc3c680d220dae31
Eric Boutueil <e.boutueil@gmail.com>
2018-02-20 16:26:19 +0100

    seeds done and crash tested

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+
+User.destroy_all
+Cloth.destroy_all
+Booking.destroy_all
+
+10.times do
+  user = User.create!(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: Faker::Internet.password(8),
+      address: Faker::Address.street_address,
+      user_picture: Faker::Avatar
+    )
+end
+puts "done users seeds"
+
+5.times do |i|
+  puts "start cloth seeds number #{i}"
+  owner = User.all.sample
+  cloth = Cloth.create!(
+      title: Faker::Lorem.sentence,
+      description: Faker::Lorem.paragraphs,
+      main_picture: Faker::LoremPixel.image("200x400"),
+      category: Cloth::CATEGORIES.sample.to_s,
+      size: Cloth::SIZES.sample.to_s,
+      price_per_day: (10..20).to_a.sample.to_i,
+      owner: owner
+    )
+
+  3.times do
+    start_date = Date.today + (1..20).to_a.sample.to_i
+    end_date = start_date + (1..5).to_a.sample.to_i
+    cloth = Cloth.all.sample
+    customer = User.all.reject{|user|user == owner}.sample
+    booking = Booking.create!(
+      start_date: start_date,
+      end_date: end_date,
+      cloth: cloth,
+      customer: customer,
+      total_booking_price: (end_date - start_date) * cloth.price_per_day
+      )
+  end
+puts "cloths and bookings seeds done"
+end
