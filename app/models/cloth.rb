c3d90876289da10fec063b7b3c1e7d060611b21a
Eric Boutueil <34768473+EricBoutueil@users.noreply.github.com>
2018-02-22 16:40:12 +0100

    Update cloth.rb
    updating CATEGORIES and SIZES

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -3,8 +3,8 @@ class Cloth < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: :owner_id
   has_many :bookings, dependent: :destroy
 
-  CATEGORIES = ["pintusrjte", "princesse", "animal"]
-  SIZES = ["S", "M", "L"]
+  CATEGORIES = ["hero", "princesse", "animal", "EJF", "EVG"]
+  SIZES = ["XS", "S", "M", "L", "XL", "Unique size"]
 
   validates :main_picture, presence: true
   validates :title, presence: true
