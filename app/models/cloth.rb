7ca7e27361e1c4330e36fe6f618c95c6e811836f
AlinaKhalilova <akhalilova@alumni.escaet.fr>
2018-02-22 15:42:28 +0100

    content-home-page

--- app/models/cloth.rb
+++ app/models/cloth.rb
@@ -2,8 +2,8 @@ class Cloth < ApplicationRecord
   belongs_to :owner, class_name: "User", foreign_key: :owner_id
   has_many :bookings, dependent: :destroy
 
-  CATEGORIES = ["a", "b", "c"]
-  SIZES = ["aa", "bb", "cc"]
+  CATEGORIES = ["pintusrjte", "princesse", "animal"]
+  SIZES = ["S", "M", "L"]
 
   validates :main_picture, presence: true
   validates :title, presence: true
